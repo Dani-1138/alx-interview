64957a1fe83a45f19a9783a2c33574df64520ba5
Dani-1138 <danieldemeke92@gmail.com>
2023-10-19 20:21:50 +0000

    stats

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,47 +1,54 @@
-#!/usr/bin/python3
-"""
-Module that parses a log and prints stats to stdout
-"""
-from sys import stdin
+#!/usr/bin/env python3
 
-status_codes = {
-    "200": 0,
-    "301": 0,
-    "400": 0,
-    "401": 0,
-    "403": 0,
-    "404": 0,
-    "405": 0,
-    "500": 0
-}
+import sys
+import signal
+from collections import defaultdict
 
-size = 0
+def print_statistics(total_size, status_counts):
+    print(f'Total file size: {total_size}')
+    for status_code in sorted(status_counts.keys()):
+        print(f'{status_code}: {status_counts[status_code]}')
 
+def process_line(line, total_size, status_counts):
+    parts = line.split()
 
-def print_stats():
-    """Prints the accumulated logs"""
-    print("File size: {}".format(size))
-    for status in sorted(status_codes.keys()):
-        if status_codes[status]:
-            print("{}: {}".format(status, status_codes[status]))
+    if len(parts) != 12 or parts[5] != '"GET' or parts[7] != 'HTTP/1.1"':
+        return total_size, status_counts
 
+    try:
+        status_code = int(parts[8])
+        file_size = int(parts[11])
+    except ValueError:
+        return total_size, status_counts
+
+    total_size += file_size
+    status_counts[status_code] += 1
+
+    return total_size, status_counts
+
+def main():
+    total_size = 0
+    status_counts = defaultdict(int)
+    lines_processed = 0
+
+    def signal_handler(sig, frame):
+        nonlocal total_size, status_counts, lines_processed
+        print_statistics(total_size, status_counts)
+        sys.exit(0)
+
+    signal.signal(signal.SIGINT, signal_handler)
+
+    try:
+        for line in sys.stdin:
+            total_size, status_counts = process_line(line.strip(), total_size, status_counts)
+            lines_processed += 1
+
+            if lines_processed % 10 == 0:
+                print_statistics(total_size, status_counts)
+
+    except KeyboardInterrupt:
+        print_statistics(total_size, status_counts)
 
 if __name__ == "__main__":
-    count = 0
-    try:
-        for line in stdin:
-            try:
-                items = line.split()
-                size += int(items[-1])
-                if items[-2] in status_codes:
-                    status_codes[items[-2]] += 1
-            except:
-                pass
-            if count == 9:
-                print_stats()
-                count = -1
-            count += 1
-    except KeyboardInterrupt:
-        print_stats()
-        raise
-    print_stats()
+    main()
+
