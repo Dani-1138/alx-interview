2990ea08fbfe4b18be943244da740e68ad029322
Dani-1138 <danieldemeke92@gmail.com>
2023-10-19 20:31:22 +0000

    stats

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,54 +1,37 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
+'''A script that reads stdin line by line and computes metrics'''
+
 
 import sys
-import signal
-from collections import defaultdict
 
-def print_statistics(total_size, status_counts):
-    print(f'Total file size: {total_size}')
-    for status_code in sorted(status_counts.keys()):
-        print(f'{status_code}: {status_counts[status_code]}')
+cache = {'200': 0, '301': 0, '400': 0, '401': 0,
+         '403': 0, '404': 0, '405': 0, '500': 0}
+total_size = 0
+counter = 0
 
-def process_line(line, total_size, status_counts):
-    parts = line.split()
+try:
+    for line in sys.stdin:
+        line_list = line.split(" ")
+        if len(line_list) > 4:
+            code = line_list[-2]
+            size = int(line_list[-1])
+            if code in cache.keys():
+                cache[code] += 1
+            total_size += size
+            counter += 1
 
-    if len(parts) != 12 or parts[5] != '"GET' or parts[7] != 'HTTP/1.1"':
-        return total_size, status_counts
+        if counter == 10:
+            counter = 0
+            print('File size: {}'.format(total_size))
+            for key, value in sorted(cache.items()):
+                if value != 0:
+                    print('{}: {}'.format(key, value))
 
-    try:
-        status_code = int(parts[8])
-        file_size = int(parts[11])
-    except ValueError:
-        return total_size, status_counts
+except Exception as err:
+    pass
 
-    total_size += file_size
-    status_counts[status_code] += 1
-
-    return total_size, status_counts
-
-def main():
-    total_size = 0
-    status_counts = defaultdict(int)
-    lines_processed = 0
-
-    def signal_handler(sig, frame):
-        nonlocal total_size, status_counts, lines_processed
-        print_statistics(total_size, status_counts)
-        sys.exit(0)
-
-    signal.signal(signal.SIGINT, signal_handler)
-
-    try:
-        for line in sys.stdin:
-            total_size, status_counts = process_line(line.strip(), total_size, status_counts)
-            lines_processed += 1
-
-            if lines_processed % 10 == 0:
-                print_statistics(total_size, status_counts)
-
-    except KeyboardInterrupt:
-        print_statistics(total_size, status_counts)
-
-if __name__ == "__main__":
-    main()
-
+finally:
+    print('File size: {}'.format(total_size))
+    for key, value in sorted(cache.items()):
+        if value != 0:
+            print('{}: {}'.format(key, value))
